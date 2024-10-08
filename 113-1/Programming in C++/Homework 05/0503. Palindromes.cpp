a20785cff93f8cf90e20fcb10e2dec53d9b96a9f
Moyu0730 <aslanchen0730@gmail.com>
2024-10-08 11:19:54 +0800

    【2024. 10. 08】0503. Palindromes - Solved

---
+++ 113-1/Programming in C++/Homework 05/0503. Palindromes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// returns true iff the subarray digits[ first .. last ] is a palindrome
+bool isPalindrome( int first, int last );
+
+int digits[ 12 ] = {};
+
+int main(){
+    int numCases;
+    cin >> numCases;
+    for( int i = 1; i <= numCases; i++ ){
+        int n;
+        cin >> n;
+
+        int length = 0;
+        for( int i = n; i > 0; i /= 10 )
+            digits[ length++ ] = i % 10;
+
+        if( isPalindrome( 0, length - 1 ) ) cout << "palindrome" << endl;
+        else cout << "non-palindrome" << endl;
+    }
+}
+
+bool isPalindrome( int first, int last ){
+    if( digits[first] != digits[last] ) return false;
+    if( last - first <= 1 ) return true;
+
+    return isPalindrome(first+1, last-1); 
+}

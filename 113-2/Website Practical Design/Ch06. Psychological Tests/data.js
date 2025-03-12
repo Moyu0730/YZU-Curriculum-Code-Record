4bcb6015761b47aebec5b8e6b9211db42417a844
Moyu0730 <aslanchen0730@gmail.com>
2025-03-12 19:53:17 +0800

    【2025. 03. 12】Class Work Finished

---
+++ 113-2/Website Practical Design/Ch06. Psychological Tests/data.js
@@ -0,0 +1,89 @@
+let questions = [
+    {
+        "question": "Are you very optimistic about this year now？",
+        "answers": [
+            ["Yes", 2],
+            ["Nope", 3],
+            ["I Don't Know", 4]
+        ]
+    },
+    {
+        "question": "Do you feel that you are becoming less and less reliable？",
+        "answers": [
+            ["Yes", 3],
+            ["Nope", 4],
+            ["Normal", 5]
+        ]
+    },
+    {
+        "question": "Do you feel happy when you think about the new year coming？",
+        "answers": [
+            ["Yes", 4],
+            ["Nope", 5],
+            ["Normal", 6]
+        ]
+    },
+    {
+        "question": "If I give you five million and ask you to leave your beloved lover, would you do it？",
+        "answers": [
+            ["Yes", 5],
+            ["It Will be All Right", 6],
+            ["Nope", 7]
+        ]
+    },
+    {
+        "question": "Have you been hit hard in the past year？",
+        "answers": [
+            ["Yes", 6],
+            ["Nope", 7],
+            ["Still Enduring It", 8]
+        ]
+    },
+    {
+        "question": "Which of the following words to describe the weather do you like？",
+        "answers": [
+            ["Beautiful Weather", 7],
+            ["The Sun is Shining", 8],
+            ["Refreshing Autumn Weather", "A"]
+        ]
+    },
+    {
+        "question": "Do you think your love has always been unusual？",
+        "answers": [
+            ["Yes", 8],
+            ["Nope", 9],
+            ["I Don't Know", 10]
+        ]
+    },
+    {
+        "question": "Where would you like to travel with your lover？",
+        "answers": [
+            ["The Blue Coast", 9],
+            ["Mountain, Forest and Lake", "A"],
+            ["Historical Town", "B"]
+        ]
+    },
+    {
+        "question": "Besides love, what do you hope to achieve this year？",
+        "answers": [
+            ["Buy A House", "C"],
+            ["Job Promotion", "D"],
+            ["Become Handsome and Beautiful", "A"]
+        ]
+    },
+    {
+        "question": "Which of the following drinks would you like to win？",
+        "answers": [
+            ["Another Bottle of Yogurt", "B"],
+            ["Another Bottle of Beer", "C"],
+            ["Another Bottle of Black Tea", "D"]
+        ]
+    }
+];
+
+let finalAnswers = {
+    "A":["Love Is a Bit Sour", "This year, your love is a little sour, but fortunately, it is not bitter. Sometimes, although it is sour, there is a hint of sweetness in it. Even if you are not destined to be together with the person you like, you can still bless each other. You feel a little sad in your heart because you are open-minded. Now I really don't care much about love. I won't force it, nor do I desire anything passionate or sweet. I just hope that after this period of bitterness, there will be sweetness."],
+    "B":["Love Is Super Sweet", "This year, your love is super sweet, really as sweet as honey. If you show your love to others from time to time, it will make people around you feel sick. Of course, you are so lucky to meet sweet love because you are a warm person. You deserve such good love and deserve others to love you well. You will also cherish each other more and give back to each other well. This is the correct way to open love."],
+    "C":["Love Has Its Bittersweet Moments", "Your love this year must have both bitterness and sweetness. In fact, this is a normal relationship, or an ordinary relationship. Some people are always sweet, they are lucky ones. But you are an ordinary person. There will be sweet moments in your relationship, but there will also be quarrels and conflicts, tears, and pain. But only if you learn to manage your relationship rationally, enjoy it when it is sweet, and examine it in time when it is bitter, can love last."],
+    "D":["Love Is Bland and Tasteless", "This year, your love life will be dull. You may not meet love at all and will continue to be single for another whole year. This is also good because you can be busy making money. Maybe your love is just like a bowl of plain boiled water, without any taste, but you can't live without it. Or maybe it's because of a lack of excitement, but whose love can always be so passionate? Being as plain as water and flowing steadily may not be a bad thing."]
+};
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # YZU-Curriculum-Code-Record
 
+## 2025. 03. 12
+
+**Class Work Finished** - 【113-2 / Website Practical Design】Ch05. Date and Time Application
+
 ## 2025. 03. 09
 
 **Solved** 【113-2 / Programming in C++】Homework 03
